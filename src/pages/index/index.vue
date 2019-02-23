4634cb0959f77163991236c33a894ed947667f1e
陈凯 <ckhero@chenkaideMacBook-Pro.local>
2019-02-23 14:35:45 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <button open-type="getUserInfo" @getuserinfo="bindGetUserInfo" @click="getUserInfo1">获取22222权限</button>
+  </div>
+</template>
+
+<script>
+  import card from '@/components/card'
+
+  export default {
+    data () {
+      return {
+        motto: 'Hello miniprograme',
+        userInfo: {
+          nickName: 'mpvue',
+          avatarUrl: 'http://mpvue.com/assets/logo.png'
+        }
+      }
+    },
+
+    components: {
+      card
+    },
+
+    mounted () {
+      this.getSetting()
+    },
+
+    methods: {
+      bindViewTap () {
+        const url = '../logs/main'
+        if (mpvuePlatform === 'wx') {
+          mpvue.switchTab({ url })
+        } else {
+          mpvue.navigateTo({ url })
+        }
+      },
+      getSetting () {
+        console.log(22222)
+        console.log(22222)
+        wx.getSetting({
+          success: function (res) {
+            if (res.authSetting['scope.userInfo']) {
+              wx.getUserInfo({
+                success: function (res) {
+                  console.log(res.userInfo)
+                  // 用户已经授权过
+                  console.log('用户已经授权过')
+                  wx.login({
+                    success (res) {
+                      if (res.code) {
+                        // 发起网络请求
+                        wx.request({
+                          url: 'http://guava.com/v1/user/login',
+                          data: {
+                            code: res.code
+                          }
+                        })
+                      } else {
+                        console.log('登录失败！' + res.errMsg)
+                      }
+                    }
+                  })
+                }
+              })
+            } else {
+              console.log('用户还未授权过')
+            }
+          }
+        })
+      },
+      getUserInfo1 () {
+        console.log('click事件首先触发')
+        // 判断小程序的API，回调，参数，组件等是否在当前版本可用。  为false 提醒用户升级微信版本
+        // console.log(wx.canIUse('button.open-type.getUserInfo'))
+        if (wx.canIUse('button.open-type.getUserInfo')) {
+          // 用户版本可用
+        } else {
+          console.log('请升级微信版本')
+        }
+      },
+      bindGetUserInfo (e) {
+        // console.log(e.mp.detail.rawData)
+        if (e.mp.detail.rawData) {
+          // 用户按了允许授权按钮
+          console.log('用户按了允许授权按钮')
+          this.getSetting()
+        } else {
+          // 用户按了拒绝按钮
+          console.log('用户按了拒绝按钮')
+        }
+      }
+    },
+
+    created () {
+      // let app = getApp()
+    }
+  }
+</script>
+
+<style scoped>
+  .userinfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .userinfo-avatar {
+    width: 128 rpx;
+    height: 128 rpx;
+    margin: 20 rpx;
+    border-radius: 50%;
+  }
+
+  .userinfo-nickname {
+    color: #aaa;
+  }
+
+  .usermotto {
+    margin-top: 150px;
+  }
+
+  .form-control {
+    display: block;
+    padding: 0 12px;
+    margin-bottom: 5px;
+    border: 1px solid #ccc;
+  }
+
+  .all {
+    width: 7.5rem;
+    height: 1rem;
+    background-color: blue;
+  }
+
+  .all:after {
+    display: block;
+    content: '';
+    clear: both;
+  }
+
+  .left {
+    float: left;
+    width: 3rem;
+    height: 1rem;
+    background-color: red;
+  }
+
+  .right {
+    float: left;
+    width: 4.5rem;
+    height: 1rem;
+    background-color: green;
+  }
+</style>
